3e68f1b8b38b12d32bb12b0ab2f12bc9556a26e9
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-05-05 19:10:29 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,12 @@ basic.forever(function () {
         if (item == true) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
             maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
-            basic.pause(700)
+            basic.pause(800)
         }
         if (item == false) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
             maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
-            basic.pause(700)
+            basic.pause(800)
         }
     } else {
         maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
